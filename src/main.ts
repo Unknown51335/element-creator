592c5c08dcb507981420410d4e69ecb94326d3e7
Unknown <49261901+Unknown51335@users.noreply.github.com>
2024-02-24 17:11:00 +0800

    feat: support function overload

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ interface Attributes {
   [qualifiedName: string]: string
 }
 
-export function createElement(
-  tagName: string,
-  attributes: Attributes = {},
-  content: string | number | boolean | HTMLElement | HTMLElement[],
-): HTMLElement {
+function isObject(obj: unknown): obj is Record<any, any> {
+  return typeof obj === 'object' && obj !== null
+}
+function isArray(obj: unknown): obj is Array<any> {
+  return Array.isArray(obj)
+}
+function isFunction(obj: unknown): obj is Function {
+  return typeof obj === 'function'
+}
+const NOOP = () => {}
+
+function _createElement(tagName: string, attributes: Attributes = {}, content: Array<any> = []): HTMLElement {
   const element: HTMLElement = document.createElement(tagName)
 
   Object.keys(attributes).forEach((key) => {
@@ -14,23 +21,41 @@ export function createElement(
     element.setAttribute(key, value)
   })
 
-  switch (typeof content) {
-    case 'string':
-      element.textContent = content
-      break
-    case 'number':
-    case 'boolean':
-    case 'bigint':
-      element.textContent = content.toString()
-      break
-    case 'object':
-      if (content instanceof HTMLElement) {
-        element.append(content)
-      } else if (Array.isArray(content)) {
-        content.forEach((item) => element.append(item))
-      }
-      break
+  for (let item of content) {
+    if (item === null || item === undefined) NOOP()
+    else if (item instanceof HTMLElement) element.append(item)
+    else if (isArray(item)) console.error('element-creator error: array is not supported')
+    else if (isObject(item)) console.error('element-creator error: object is not supported')
+    else if (isFunction(item)) console.error('element-creator error: function is not supported')
+    else {
+      item = String(item)
+      element.append(item)
+    }
   }
 
   return element
+}
+
+export function createElement(tagName: string, attributes: Attributes, content?: any): HTMLElement
+export function createElement(tagName: string, content?: any): HTMLElement
+export function createElement(tagName: string, attributesOrContent?: any, content?: any): HTMLElement {
+  const argumentsLength = arguments.length
+
+  let _content = content
+
+  if (argumentsLength === 1) {
+    return _createElement(tagName, {}, [])
+  } else if (argumentsLength === 2) {
+    if (isArray(attributesOrContent)) return _createElement(tagName, {}, attributesOrContent)
+    if (isObject(attributesOrContent)) return _createElement(tagName, attributesOrContent, [])
+    return _createElement(tagName, {}, [attributesOrContent])
+  } else if (argumentsLength === 3) {
+    if (isArray(_content)) {
+      return _createElement(tagName, attributesOrContent, _content)
+    }
+    return _createElement(tagName, attributesOrContent, [_content])
+  } else {
+    _content = Array.prototype.slice.call(arguments, 2)
+    return _createElement(tagName, attributesOrContent, _content)
+  }
 }
